0ec7b79b84752d15be55205705e8a4f9ffdf9c8d
pH2O4 <ph2oalencar@gmail.com>
2022-03-23 00:28:11 -0300

    ---

--- my-app/src/componentes/Users.jsx
+++ my-app/src/componentes/Users.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import './Users.css'
 import {Button} from 'react-bootstrap'
+import Axios from 'axios'
 
 const Users = () => {
 
+  window.onload = () => {
+    Axios.post('http://localhost:3005/api/GetUserInformations', {
+      EmailX: localStorage.Email,
+
+    }).then((response) => {
+      console.log(response.data)
+const contato = response.data.Contato
+const email = response.data.Email
+const name = response.data.Name
+    })
+    }
 
   return(
     <div className="Users">
 <div>
 <div className="UserInformation">
-<div className="UserInformationComponent">User Name</div>
-<div className="UserInformationComponent">Contato: </div>
-<div className="UserInformationComponent">Email: </div>
+<div className="UserInformationComponent">User Name:{contato}</div>
+<div className="UserInformationComponent">Contato:{email} </div>
+<div className="UserInformationComponent">Email: {name}</div>
 <div> <Button variant="primary">Delete My Count</Button></div>
 </div>
 </div>
